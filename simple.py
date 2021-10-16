472798084ff823568a67d7f22d5545df1c169c68
phildoc <92617461+phildoc@users.noreply.github.com>
2021-10-16 12:09:43 +0200

    Update simple.py

--- simple.py
+++ simple.py
@@ -4,5 +4,23 @@ import random
 
 print "Content-Type: text/plain"
 print
+##value = random.randint(10000+10, 99999999)
+##print value
+##
+##while value %10 != 0:
+##    value -= 1
+##
+##print "Random value is " + str(value);
 
-print "Random value is " + str(random.randint(100000, 999999));
+print '<html>'
+print '<head>'
+print '<title>test Python dynamic html</title>'
+print '</head>'
+print '<body>'
+value = random.randint(10000+10, 99999999)
+print value
+
+while value %10 != 0:
+    value -= 1
+<p>print "Random value is " + str(value);</p>
+print '</body>'
