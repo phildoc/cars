b3c6d43c848dcb35f0c7745577dfbc099a8f4cc2
phildoc <92617461+phildoc@users.noreply.github.com>
2021-10-18 05:35:20 +0200

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python
-
-print "Content-Type: text/plain"
+print "Content-Type: text/html"
 print
-import random
-mod = 1999
-value = random.randint(1000+mod, 99999999)
-print value
-
-
-while value %mod != 0:
-    value -= 1
-
-print "Random value is " + str(value);
+print 'Hello World'
